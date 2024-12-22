475c09fc4b828eec6b9d40829811e0d60748980e
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-12-22 02:13:53 +0000

    19 pt 2

--- days/19/19.ts
+++ days/19/19.ts
@@ -40,7 +40,11 @@ function two(data: string) {
   const patterns = data.split("\n\n")[0].split(", ");
   const designs = data.split("\n\n")[1].split("\n");
 
+  let memo: Record<string, number> = {};
+
   function attempt(current: string, desired: string): number {
+    const k = `${current}-${desired}`;
+    if (memo[k]) return memo[k];
     let res = 0;
 
     for (const p of patterns) {
@@ -50,12 +54,14 @@ function two(data: string) {
         res += attempt(concat, desired);
       }
     }
+
+    memo[k] = res;
     return res;
   }
 
   let possible = 0;
   for (const design of designs) {
-    console.log(designs.indexOf(design));
+    // console.log(designs.indexOf(design));
     possible += attempt("", design);
   }
 
